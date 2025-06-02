dbf48a26921c16637903d27bca8b6f0fa1cf91e7
Justus <justus.d2025@gmail.com>
2025-06-02 22:59:36 +0200

    added byte viewer

--- if_bmp.py
+++ if_bmp.py
@@ -1,5 +1,65 @@
 
+def better_byte_viewer(data: bytes) -> None:
+    pr_string = ""
+    
+    left, right = [], []
+    
+    for index, byte in enumerate(data):
+        char = chr(byte)
+            
+        pr_string += f"{char if char.isprintable() else '.'}"
+        if (index + 1) % 16 == 0 and pr_string:
+            right.append(pr_string)
+            pr_string = ""
+    else:
+        if pr_string:
 
+            right.append(pr_string)
+            pr_string = ""
+            
+    for index, byte in enumerate(data):
+        
+        if byte < 16:
+            num = f'0{hex(byte)[2:].upper()}'
+        else:
+            num = f'{hex(byte)[2:].upper()}'
+        pr_string += f"{num} "
+        if (index + 1) % 16 == 0 and pr_string:
+            left.append(pr_string)
+            pr_string = ""
+    else:
+        if pr_string:
+            left.append(pr_string)
+    
+    for l,r in zip(left,right):
+        print(f"{l:<48}{r}")
+
+def bit_viewer(data: bytes):
+    pr_string = ""
+    for index, byte in enumerate(data):
+        char = chr(byte)
+            
+        pr_string += f"{char if char.isprintable() else '.'}"
+        if index % 16 == 0 and pr_string:
+            print(pr_string)
+            pr_string = ""
+    else:
+        if pr_string:
+            print(pr_string)
+    pr_string = ""
+    for index, byte in enumerate(data):
+        
+        if byte < 16:
+            num = f'0{hex(byte)[2:].upper()}'
+        else:
+            num = f'{hex(byte)[2:].upper()}'
+        pr_string += f"{num} "
+        if (index + 1) % 16 == 0 and pr_string:
+            print(pr_string)
+            pr_string = ""
+    else:
+        if pr_string:
+            print(pr_string)
 
 def loader(file_name: str):
     # BitMap File Header
@@ -7,7 +67,7 @@ def loader(file_name: str):
     assert file_name.endswith('.bmp')
     with open(file_name, 'rb') as file:
         image = file.read()
-    
+    better_byte_viewer(image)
     # Header
     
     signature = image[:2]
